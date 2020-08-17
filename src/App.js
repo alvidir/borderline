2cd11eebd166046c2c29646be60645475bb0d180
Hector <hector.morales.carnice@gmail.com>
2020-08-17 21:37:46 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import GoogleButton from "./components/google/button";
+import Wallpaper from "./components/wallpaper/view";
 
 function App() {
-  return <GoogleButton />;
+  return (
+  <div>
+    <GoogleButton />
+    <Wallpaper/>
+  </div>
+  );
 }
 
 export default App;
---
+++ src/components/wallpaper/view.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import * as unsplash from '../../proto/unsplash/api_pb';
+
+class View extends Component {
+    state = {};
+}
+
+export default View;
